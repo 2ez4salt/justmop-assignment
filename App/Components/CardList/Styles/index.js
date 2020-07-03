3e559843092b6deca5ab4fc580775dd75b22b1fd
Talha <talha@talhasalt.com.tr>
2020-07-03 06:02:32 +0300

    styles added/refactored

---
+++ App/Components/CardList/Styles/index.js
@@ -0,0 +1,31 @@
+import {StyleSheet} from 'react-native';
+import {Dimensions} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    width: 400,
+    height: 548,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    margin: 10,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  text: {
+    fontSize: 30,
+  },
+  searchText: {
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+    borderWidth: 0.5,
+    margin: 10,
+    marginTop: 25,
+    marginBottom: 5,
+  },
+});
---
+++ App/Containers/Mechanics/Styles/index.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#dcab60',
+    justifyContent: 'center',
+  },
+  mech: {
+    borderRadius: 10,
+    borderWidth: 0.5,
+    padding: 10,
+    margin: 5,
+    backgroundColor: '#dfdfd',
+    alignSelf: 'stretch',
+  },
+  text: {
+    fontSize: 30,
+  },
+  searchText: {
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+    borderWidth: 0.5,
+    margin: 10,
+    marginTop: 25,
+    marginBottom: 5,
+  },
+});
